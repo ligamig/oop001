0b640c4a7995a5c7aa79e533a4eb7b88f198ffd5
Līga Migoļa <liga.migola@ventspils.edu.lv>
2025-01-15 15:21:51 +0200

    hallo

---
+++ op001.py
@@ -0,0 +1,8 @@
+class Dzivnieks:
+    def __init__(self, name, kajas, skanja):
+        self.name = name
+        self.kajas = kajas
+        self.skanja = skanja
+
+p1 = Dzivnieks("Kakis", 4, "meow")
+print(p1.name, p1.kajas)
