baf14f4022c9683cd283d4fa80b65600ef5d0d24
Līga Migoļa <liga.migola@ventspils.edu.lv>
2025-01-16 09:37:29 +0200

    4

--- op001.py
+++ op001.py
@@ -1,7 +1,10 @@
+from abc import ABC,abstractmethod
+
 class Dzivnieks:
     def __init__(self, name, kajas):
         self.name = name
         self.kajas = kajas
+    @abstractmethod
     def skanja(self):
         print("random animal noise")
 #        return self.skanja
@@ -11,24 +14,56 @@ class Dzivnieks:
 class Suns(Dzivnieks):
     def __init__(self, name, kajas):
         super().__init__(name, kajas)
-        self.name="Komisars"+self.name
+        self.name="Komisars "+self.name
     def skanja(self):
         print("vau vau")
 
-d1 = Dzivnieks("Gauja", 4)
-print(d1)
-d1.skanja()
+class Kakis(Dzivnieks):
+    def __init__(self, name, kajas):
+        super().__init__(name, kajas)
+        self.name="Kaiminu "+self.name
+    def skanja(self):
+        print("meow meow rrrrrrrrrrrr")
 
-s1 = Suns("Reksis", 4)
-print(s1)
-s1.skanja()
+class Govs(Dzivnieks):
+    def __init__(self, name, kajas):
+        super().__init__(name, kajas)
+        self.name="Milkere "+self.name
+    def skanja(self):
+        print("moooooooooooo")
 
-#class Suns(Dzivnieks):
+dzivniekuSaraksts = []
+dzivniekuSaraksts.append(Suns("Reksis", 3))
+dzivniekuSaraksts.append(Suns("Volvis", 4))
+dzivniekuSaraksts.append(Suns("Jimmys", 4))
+dzivniekuSaraksts.append(Kakis("Muris", 4))
+dzivniekuSaraksts.append(Kakis("Juris", 5))
+dzivniekuSaraksts.append(Govs("Gauja", 4))
+
+print("####################")
+for dzivnieks in dzivniekuSaraksts:
+    print(dzivnieks)
+    dzivnieks.skanja()
+
+
+#     d1 = Dzivnieks("Gauja", 4)
+# print(d1)
+# d1.skanja()
+
+# s1 = Suns("Reksis", 4)
+# print(s1)
+# s1.skanja()
+
+# s2 = Kakis("Minkans", 4)
+# print(s2)
+# s2.skanja()
+
+# class Suns(Dzivnieks):
 #    def __init__(self, name, kajas):
 #        super().__init__(name, kajas)
 #        self.skanja = "vua"
 
-#class Kakis(Dzivnieks):
+# class Kakis(Dzivnieks):
 #    def __init__(self, name, kajas):
 #        super().__init__(name, kajas)
 #        self.skanja = "mjau"
